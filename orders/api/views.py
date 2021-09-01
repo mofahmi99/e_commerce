f78acaeae32c63bc86a75a30b8c42df1f68e604f
mofahmi99 <m.fahmy@sary.com>
2021-09-01 23:50:31 +0200

    updated create order api to take address_id as a query parameter to be inserted while creating the order

--- orders/api/views.py
+++ orders/api/views.py
@@ -25,14 +25,16 @@ class OrderApi(GenericViewSet):
     @action(methods=['post'], detail=False)
     def create_order(self, request):
         """
-        create order
+        create order takes address_id as query parameter to be inserted while creating the order
         """
         data = request.data
+        address_id = request.query_params["address_id"]
+        # data._mutable = True
         data["user"] = request.user.id
+        data["address"] = address_id
         serializer = OrderCreateSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message": serializer.data, "status": True}, status=status.HTTP_200_OK)
+            return Response({"message": serializer.data, "status": True}, status=status.HTTP_201_CREATED)
         return Response({"result": serializer.errors, "message": "Done", "status": False},
                         status=status.HTTP_400_BAD_REQUEST)
-
